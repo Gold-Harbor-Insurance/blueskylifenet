b4d034303f0ad5a9ba321c848b43fcabdf0c295e
Seth Bailey <48031387-Seth220@users.noreply.replit.com>
2025-10-07 00:08:20 +0000

    Configure base path for subdirectory deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/final-expense/rb-dhF5ke48DSslf/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
